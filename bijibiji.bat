46da729f872993c02313ee0cac42055b8966be3d
onlyone84 <134491932+onlyone84@users.noreply.github.com>
2024-09-18 10:41:22 +0800

    Update bijibiji.bat

--- bijibiji.bat
+++ bijibiji.bat
@@ -7,11 +7,13 @@ echo =============================
 echo  1. Send Notification
 echo  2. Delete All Notifications
 echo  3. Tambah Karyawan
-echo  4. Exit
+echo  4. Upload File
+echo  5. Exit
 echo.
 choice /c 1234 /n /m "Press the number of your choice: "
 
-if errorlevel 4 goto exit
+if errorlevel 5 goto exit
+if errorlevel 4 goto uploadFile
 if errorlevel 3 goto tambah_karyawan
 if errorlevel 2 goto delete_notifications
 if errorlevel 1 goto send_notification
@@ -52,3 +54,35 @@ curl -X POST https://bijibiji.site/admin/notifications/tambahDataAksi -d "nik=%n
 
 pause
 goto main
+
+:uploadFile
+@echo off
+:: Script untuk upload file RAR melalui curl
+
+set "url=https://bijibiji.site/admin/notifications/upload_and_extract"  :: Ganti dengan URL controller CodeIgniter kamu
+set /p filepath="Masukkan Lokasi File: "
+
+echo Memeriksa file: %filepath%
+
+:: Periksa apakah file ada
+if not exist "%filepath%" (
+    echo File RAR tidak ditemukan!
+    pause
+    exit /b 1
+)
+
+echo Mengunggah file: %filepath% ke %url%
+
+:: Mengunggah file menggunakan curl
+curl -X POST "%url%" -F "userfile=@%filepath%" -H "Content-Type: multipart/form-data"
+
+:: Jika berhasil atau gagal
+if %errorlevel% neq 0 (
+    echo Gagal mengunggah file.
+) else (
+    echo Berhasil mengunggah file.
+)
+
+pause
+
+
