8aa3f618b1154d83291552a59287503729b2e3ef
onlyone84 <134491932+onlyone84@users.noreply.github.com>
2024-09-21 14:47:50 +0800

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,16 @@
-const inputSearch = document.getElementById("search")
-const search = "https://google.com/search?q="
+const inputSearch = document.getElementById("search");
+const search = "https://google.com/search?q=";
 
 function searchGoogle() {
-  location.replace(search + inputSearch.value)
+  location.replace(search + inputSearch.value);
 }
 
 function sayaBeruntung() {
-  location.replace('https://github.com/onlyone84')
+  location.replace('https://github.com/onlyone84');
 }
+
+inputSearch.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    searchGoogle();
+  }
+});
