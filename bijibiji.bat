60efae898b84ad9fbbfb758ab7bc2e44d42b4ff0
onlyone84 <134491932+onlyone84@users.noreply.github.com>
2025-03-11 08:57:34 +0800

    Update bijibiji.bat

--- bijibiji.bat
+++ bijibiji.bat
@@ -2,7 +2,7 @@
 :login
 cls
 echo ==============================
-echo           Login System..
+echo           Login System..s
 echo ==============================
 set /p nik="     Masukkan NIK     : "
 set /p password="Masukkan Password: "
